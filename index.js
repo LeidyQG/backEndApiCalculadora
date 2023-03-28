c68e59f2058f8a489ad942c8f5e6ab444fe4fd48
Leidy Quiñones <lalis.181014@gmail.com>
2023-03-28 16:50:36 -0500

    versión 0.0.3

--- index.js
+++ index.js
@@ -1,63 +1,61 @@
-// 1. Crear un objeto express que exprese la biblioteca express
+//1. Crear un objeto express
 const express=require('express');
 
-// 2. Crear un objeto que represente nuestra aplicación
+//2. Crear un objeto que represente nuestra aplicación
 const app= express();
+
 app.use(express.json());
 
-//Definir los puntos de (entrypoint)entrada(URL) en donde va a responder la Api
-//http://localhost:3000   =====> http://localhost:3000/sumar ruta de la Api
+//Definir los entry point de la API
+//Definir la ruta (la url) en dónde va a responder nuestra API
+// http://localhost:3000 =====> http://localhost:3000/api/sumar
+
+
 app.get(
-    '/sumar',
-    //Se requiere dos objetos: uno representando la petición y el otro la respusta en la función de callback
-    (req,res)=>{
-        //Procesamiento de la petición
-        console.log("Alguein está en la ruta sumar");
+    '/api/sumar',
+    //Se requieren dos objetos: uno representando la petición
+    //un objeto representando la respuesta
+    (req, res)=>{
+        console.log("Alguien está conectándose a la ruta sumar");        
         res.json(req.body);
     }
 );
 
 app.post(
-    '/restar',
-    (req,res)=>{
-
-        const {numero_uno, numero_dos}=req.body;
-        /*const uno=req.body.numero_uno;
-        const dos=req.body.numero_dos;*/
-        const resultado= numero_uno-numero_dos;
+    '/api/restar',
+    (req, res)=>{
+        const {numero_1, numero_2}= req.body;
+        const resultado=numero_1-numero_2;        
         res.json(resultado);
     }
 );
 
-//¿cómo queda la ruta para dividir?
+//¿Cómo queda la ruta para dividir?
 
 app.post(
-    '/divir',
+    '/api/dividir',
     (req,res)=>{
         let resultado;
+        
         try{
-            const {numero_uno, numero_dos}=req.body;
-            resultado= numero_uno/numero_dos;
+            const {numero_1, numero_2}= req.body;           
+            resultado=numero_1/numero_2;   
+
         }catch(error){
             //Gestionar el error
             resultado=error;
         }
+        
         res.json(resultado);
     }
-);
+)
 
-//"3". Crear un servicio para escuchar peticiones
 
+
+//3. Crear un servicio para escuchar peticiones
 app.listen(
-    3000,
+    3001,
     ()=>{
-        console.log("Servidor ejecutándose en el puerto 3000 con éxito")
+      console.log("Servidor ejecutándose en el puerto 3001 con éxito");
     }
 );
-
-
-
-
-
-
-
